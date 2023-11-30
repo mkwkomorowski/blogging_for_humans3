acecffae5d7987a8aae494644c60e3b412652000
mkwkomorowski <149168149+mkwkomorowski@users.noreply.github.com>
2023-11-30 15:51:33 +0100

    Update views.py

--- blogging_for_humans_3/views.py
+++ blogging_for_humans_3/views.py
@@ -6,10 +6,4 @@ def return_first_model_if_exists(request):
     queryset = models.Model123.objects.all()
     list_of_models = list(queryset)
     result = list_of_models[0].id if list_of_models else 'abcd'
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-    print(result)
-    print(type(result))
-    print(result.id)
-    print(type(result.id))
-    print('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
-    return HttpResponse(result)
+    return HttpResponse(str(result))
