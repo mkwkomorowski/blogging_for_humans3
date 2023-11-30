6361461d4f7ffdbf3e22b601b40a1d5af794ef3b
mkwkomorowski <149168149+mkwkomorowski@users.noreply.github.com>
2023-11-30 12:53:49 +0100

    Update views.py

--- blogging_for_humans_3/views.py
+++ blogging_for_humans_3/views.py
@@ -5,9 +5,11 @@ from django.http import HttpResponse
 def return_first_model_if_exists(request):
     queryset = models.Model123.objects.all()
     list_of_models = list(queryset)
-    result = list_of_models[0] if list_of_models else 'abcd'
+    result = list_of_models[0].id if list_of_models else 'abcd'
     print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
     print(result)
     print(type(result))
+    print(result.id)
+    print(type(result.id))
     print('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
     return HttpResponse(result)
