07d5326cc2c210738b9454f734960fe02f223958
developer <edxapp@plisi-dev7.novalocal>
2023-11-30 10:03:53 +0000

    correction

--- blogging_for_humans_3/views.py
+++ blogging_for_humans_3/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 def return_first_model_if_exists(request):
     queryset = models.Model123.objects.all()
-    result = list(queryset)[0] or 'abcd'
+    list_of_models = list(queryset)
+    result = list_of_models[0] if list_of_models else 'abcd'
     return HttpResponse("result: " + (str(result)))
