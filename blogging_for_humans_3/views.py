d32493181690c429d95cfcd9bd6481d622e9f542
developer <edxapp@plisi-dev7.novalocal>
2023-11-30 10:49:58 +0000

    mod

--- blogging_for_humans_3/views.py
+++ blogging_for_humans_3/views.py
@@ -6,4 +6,7 @@ def return_first_model_if_exists(request):
     queryset = models.Model123.objects.all()
     list_of_models = list(queryset)
     result = list_of_models[0] if list_of_models else 'abcd'
+    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    print(result)
+    print('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
     return HttpResponse("result: " + (str(result)))
